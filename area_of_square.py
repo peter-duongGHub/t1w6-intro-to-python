45127bf2a8e58aaa4526817233356bfbf5013776
peter-duongGhub <pduong197@icloud.com>
2024-06-04 21:03:38 +0100

    programs with inputs

---
+++ area_of_square.py
@@ -0,0 +1,7 @@
+#get the length from the user
+length = float(input("Enter the length of the square: "))
+print(length)
+#calculate and store the area in a variable, area = length * length
+area = length * length
+#print the calculated area to the terminal
+print(f"The calculated area is {area}")
---
+++ create_email_form.py
@@ -0,0 +1,8 @@
+#Peter Duong Coder Academy -> jairo.bilbao@coderacademy.com
+first_name = input("Please, enter your first name: ")
+last_name = input("Please, enter your last name: ")
+company_name = input("Please, enter your company name(no blank spaces): ")
+print(f"{first_name}.{last_name}@{company_name}.com".lower())
+
+email = f"{first_name}.{last_name}@{company_name}.com".lower()
+print(email)
