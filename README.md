76bcef19983beb66c43004b8b2ea80deb1efb877
peter-duongGhub <pduong197@icloud.com>
2024-06-04 20:29:28 +0100

    data types

---
+++ README.md
@@ -0,0 +1,31 @@
+# Intro to Python
+
+## Programming Language
+
+- A language that writes programs for computers.
+
+## Program
+
+- Set of instructions that computers know how to execute 
+- Converts programming language commands /instructions in machine language.
+
+## Data types
+<!-- Primitive Data Types -->
+- Different data
+- Depends on the program language
+- Integers (1, 2, 3, 0, -5, 12312)
+- Floats (3.5, -4.12, .7.124124)
+- Strings ("Hello", '1234', "", ...)
+- Boolean (True/False, 1/0)
+<!-- Non primitive Data Types -->
+
+## Variables
+
+- Container for our piece of data 
+- Whenever we want that data, we can use the variables
+- Change the variable value as we want
+
+## REPL
+<!-- Python Interpreter -->
+- Read, Evaluate, Print, Loop
+<!-- Exit python by exit() or quit() -->
--- main.py
+++ main.py
@@ -1,3 +1,65 @@
 print("Hello world!!")
 print(5 - 3)
 print("Don't forget to commit!")
+
+# print ("It's a comment, it won't be printed")
+
+# Data types
+# integers
+# 4
+# 7
+# 9
+# print(9)
+# print("9")
+# print(type(9))
+# print(type("9"))
+
+# #floats
+# 2.5
+# 5.123123
+# print(3.1415)
+# print(type(-5.63))
+# print(type(-5))
+
+#complex (imaginary numbers)
+2 + 3j 
+print(type(4 + 7j))
+
+#operators + - * / 
+# print (4 * 5)
+# print (15 / 3)
+# print (15 / 2)
+# print(7 % 2)
+
+#String
+# "Hello world"
+# 'Hello'
+# print("This is a string")
+
+#String concatenations, addition, join
+# print("Hello " + "World")
+# print(3 + 5) 
+# print("3" + "5")
+
+#boolean
+# Capitalised
+True
+False
+print(type(False))
+0 # falsy value
+"" # falsy value
+print(bool(0))
+print(bool(""))
+print(bool(-123))
+print(bool("False"))
+
+#comparison operators <, >, <=, >=, ==, !=
+# print(2 < 3)
+# print(5 >= 5)
+# print("HELLO".lower())
+# print("Hello".lower() == "hello")
+
+#logical operators and, or, not
+print(True and False) #AND returns True when both operators are True
+print(False or True) #OR returns True, when one or both are True
+print(not False) #NOT returns the opposite value 
